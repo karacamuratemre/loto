46cc3a8021c513b14f1a24e94738a58c3ac016b2
karacamuratemre <karacamuratemre@gmail.com>
2024-08-29 18:15:59 +0300

    superLoto-Sayısal Loto

---
+++ src/main/java/com/example/superloto/SuperLotoApplication.java
@@ -0,0 +1,13 @@
+package com.example.superloto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperLotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperLotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/superloto/SuperLotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superloto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperLotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
